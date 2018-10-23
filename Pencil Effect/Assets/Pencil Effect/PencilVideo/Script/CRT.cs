16a0493084e15e24d906dda00f8425b32c6bfa7b
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 15:56:17 +0200

    Add Pencil-Effect Project

---
+++ Pencil Effect/Assets/Pencil Effect/PencilVideo/Script/CRT.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CRT : MonoBehaviour
+{
+    public static Texture2D renderedTexture;
+    public Material Mat;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Mat);
+    }
+}
